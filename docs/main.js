ac1c3b1c66d052385a953d14af7a63a0a4edd0d6
GreenNope <tlash1000@gmail.com>
2018-08-29 12:42:03 -0400

    Update main.js

--- docs/main.js
+++ docs/main.js
@@ -3,8 +3,8 @@ var gold = 0
 var xp = 0
 var opponentHP = 10
 var opponentLevel = 1
-var opponentHPDisplay = Document.getElementById("opponentHP")
-var opponentMaxHPDisplay = Document.getElementById("opponentMaxHP")
+var opponentHPDisplay = document.getElementById("opponentHP")
+var opponentMaxHPDisplay = document.getElementById("opponentMaxHP")
 function class1Select(IDvalue) {
   console.log("Class 1: " + classValues[IDvalue] + "(" + IDvalue + ")")
 }
@@ -21,5 +21,4 @@ function newOpponentSpawn () {
   opponentHP = 1.01^opponentLevel 
   opponentHPDisplay.innerHTML = opponentHP
   opponentMaxHPDisplay.innerHTML = opponentHP
-  
 }
