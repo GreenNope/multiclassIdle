5362e0aaa4bd995ea0811f36b6b1f5956403310c
GreenNope <tlash1000@gmail.com>
2018-08-29 12:25:11 -0400

    Update main.js

--- docs/main.js
+++ docs/main.js
@@ -3,15 +3,20 @@ var gold = 0
 var xp = 0
 var opponentHP = 10
 var opponentLevel = 1
+var opponentHPDisplay = Document.getElementById("opponentHP")
+var opponentMaxHPDisplay = Document.getElementById("opponentMaxHP")
 function class1Select(IDvalue) {
   print("Class 1: " + classValues[IDvalue] + "(" + IDvalue + ")")
 }
 
-function attackOpponent(damage) {
-  opponentHP = opponentHP - damage 
-  
+function damageOpponent(damage) {
+  opponentHP = opponentHP - damage
+  opponentHPDisplay.innerHTML = opponentHP
 }
 
 function newOpponentSpawn () {
   opponentHP = 1.01^opponentLevel 
+  opponentHPDisplay.innerHTML = opponentHP
+  opponentMaxHPDisplay.innerHTML = opponentHP
+  
 }
