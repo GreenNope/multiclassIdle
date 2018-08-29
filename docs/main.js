3f3538ffba3d448aeb89e1dadc177b2a4a0f8f42
GreenNope <tlash1000@gmail.com>
2018-08-29 12:10:54 -0400

    Update main.js

--- docs/main.js
+++ docs/main.js
@@ -1,5 +1,13 @@
 classValues = ["Fighter"]
-
+var gold = 0
+var xp = 0
+var opponentHP = 10
+var opponentLevel = 1
 function class1Select(IDvalue) (
   print("Class 1: " + classValues[IDvalue] + "(" + IDvalue + ")")
 )
+
+function newOpponentSpawn ()
+  opponentHP = 1.01^opponentLevel 
+
+end
