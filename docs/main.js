3d39b1a8537bbf60effe45d3f8d45f9c752eaa18
GreenNope <tlash1000@gmail.com>
2018-08-29 12:12:36 -0400

    Update main.js

--- docs/main.js
+++ docs/main.js
@@ -3,11 +3,15 @@ var gold = 0
 var xp = 0
 var opponentHP = 10
 var opponentLevel = 1
-function class1Select(IDvalue) (
+function class1Select(IDvalue) {
   print("Class 1: " + classValues[IDvalue] + "(" + IDvalue + ")")
-)
+}
 
-function newOpponentSpawn ()
+function attackOpponent(damage) {
+  opponentHP = opponentHP - damage 
+  
+}
+
+function newOpponentSpawn () {
   opponentHP = 1.01^opponentLevel 
-
-end
+}
